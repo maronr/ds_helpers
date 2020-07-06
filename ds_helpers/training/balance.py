b9340127f6bf72556f9cb9970edcdf79a703b7f0
maronr <max@famschmitt.com>
2020-07-06 17:02:44 +0000

    add upsample and balance functions

--- ds_helpers/training/balance.py
+++ ds_helpers/training/balance.py
@@ -7,31 +7,11 @@ import pandas as pd
 import numpy as np
 
 # Cell
-def downsample(df:pd.DataFrame, y_column:str, random_state:int, min_size:int=None, **kwargs) -> pd.DataFrame:
+def downsample(df:pd.DataFrame, y_column:str, min_size:int=None, random_state:int=115, **kwargs) -> pd.DataFrame:
     '''Balance classes of the target variable by downsampling all classes to be equal to or smaller than "min_size".
 
     Classes smaller than "min_size" are not affected and will remain at their current size. If "min_size" is ommitted,
     the size of the smallest current class is taken as "min_size".
-
-    Paramters
-
-    df : pandas dataframe
-         Dataframe containing column "y_column".
-
-    y_column : str
-               Name of df column containing the target variable (label).
-
-    ranomd_state : int
-                   Random state for reproducibility.
-
-    min_size : int, default=None
-               If no value is supplied, min_size will be set to the size of the smallest current class.
-
-    Returns
-
-    new_df : pandas dataframe
-             Has the same structure as the input dataframe but classes were balanced by downsampling.
-
     '''
     df_new = df.copy()
 
